a750b5482594e160b10b6561c9c372877724ed59
leonda <darialeon121999@gmail.com>
2024-05-16 21:58:48 -0400

    method overloading, var args, type promotion

--- src/my_utilities/ArrayUtil.java
+++ src/my_utilities/ArrayUtil.java
@@ -101,4 +101,104 @@ public class ArrayUtil {
 
         return false;
     }
+
+
+
+
+    /**
+     * This method returns new array with added element at the end
+     * @param origArr is an array where we will add an element to the end
+     * @param elemToAdd is the element we will add to the end
+     * @return new array with added element
+     */
+
+
+    public static int [] addElemInArr (int [] origArr, int elemToAdd){
+
+        int [] newArr = Arrays.copyOf(origArr, origArr.length + 1); //creating 1 more additional space in the new copied array
+        // {1, 2, 3, _};
+
+        newArr[newArr.length-1] = elemToAdd;
+
+
+
+        return newArr;
+
+
+
+    }
+
+
+    //TODO: create a reusable method that takes 2 int arrays and returns one array combined
+
+
+
+    /**
+     * This methods finds the first matched element's index of int array
+     * @param arr is the parameter where we will check for the element
+     * @param num is the parameter where we will check if there is a match to it
+     * @return the index if there is a match and -1 if there is no match.
+     */
+
+
+
+    public static int indexOf (int [] arr, int num){
+
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] == num){
+                return i;
+            }
+
+        }
+
+        return  -1;
+
+    }
+
+
+    /**
+     * This methods finds the first matched element's index of String array
+     * @param array is the parameter where we will check for the element
+     * @param word is the parameter where we will check if there is a match to it
+     * @return the index if there is a match and -1 if there is no match.
+     */
+
+
+    public static int indexOf (String [] array, String word){
+
+
+        for (int i = 0; i < array.length; i++) {
+
+            if (array[i].equals(word)){
+                return i;
+            }
+
+        }
+
+        return  -1;
+
+    }
+
+
+
+    /**
+     * This method sums up all elements in int array
+     * @param nums is the array where I get all the elements to sum up
+     * @return the total of the all elements summed up.
+     */
+    public static int sum (int ... nums){ // ... --> var args , will be accepting these parameters as int array
+
+        int sum = 0;
+
+        for (int each : nums){
+            sum += each;
+
+        }
+
+        return sum;
+    }
+
+
 }
