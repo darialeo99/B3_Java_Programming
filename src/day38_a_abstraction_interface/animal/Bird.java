562be800957968230e6b65e4ad845495b187f93c
leonda <darialeon121999@gmail.com>
2024-06-13 22:19:53 -0400

    recap of abstraction , interface, new topic polymopfism

---
+++ src/day38_a_abstraction_interface/animal/Bird.java
@@ -0,0 +1,9 @@
+package day38_a_abstraction_interface.animal;
+
+public abstract class Bird extends Animal {
+
+    double beakSize;
+
+
+    public abstract void useWings();
+}
---
+++ src/day38_a_abstraction_interface/shopping/Shipping.java
@@ -0,0 +1,8 @@
+package day38_a_abstraction_interface.shopping;
+
+public interface Shipping {
+
+    String COUNTRY = "US";
+
+    void payForShipping(boolean hasPrime);
+}
